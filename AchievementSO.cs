d3915f98e55aeb1ecf7bf622f9203dd15b0fbf13
Matheus Duca <63525232+m-duca@users.noreply.github.com>
2026-05-12 15:23:42 -0300

    import: Scriptable for achievements configuration

---
+++ AchievementSO.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+/// <summary>
+/// Store achievement's data
+/// </summary>
+[CreateAssetMenu(fileName = "AchievementSO_", menuName = "Achievements/AchievementSO")]
+public class AchievementSO : ScriptableObject
+{
+    [Heade(("Settings"))]
+    [SerializeField] private string _saveID;
+    [SerializeField] private string _achievementName;
+    [SerializeField, TextArea()] private string _description;
+    [SerializeField] private Sprite _iconSprite;
+    [SerializeField] private int _targetProgressValue;
+
+    // Properties
+    public string SaveID { get { return _saveID.ToLower(); } }
+    public string AchievementName { get { return _achievementName; } }
+    public string Description { get { return _description; } }
+    public Sprite IconSprite { get { return _iconSprite; } }
+    public int TargetProgressValue { get { return _targetProgressValue;} }
+}
